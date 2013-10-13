151e1f4eceeb7b7bf4c469f3e7b7238396d46822
Christopher Backhouse <bckhouse@caltech.edu>
2013-10-12 20:09:05 -0700

    Add peal time fields

--- gui.py
+++ gui.py
@@ -20,8 +20,7 @@ vbox = gtk.VBox()
 
 hbox = gtk.HBox()
 
-label = gtk.Label('Method:')
-hbox.pack_start(label, False)
+hbox.pack_start(gtk.Label('Method:'), False)
 
 method = gtk.combo_box_new_text()
 method.append_text('Plain Hunt')
@@ -41,8 +40,28 @@ hbox.add(stage)
 vbox.add(hbox)
 
 hbox2 = gtk.HBox()
-hbox2.pack_end(start, False)
+hbox2.pack_start(gtk.Label('Peal speed:'), False)
+
+hours = gtk.SpinButton()
+hours.set_numeric(True)
+hours.set_range(0, 5)
+hours.set_value(2)
+hours.set_increments(1, 1)
+hbox2.add(hours)
+hbox2.pack_start(gtk.Label('hr'), False)
+minutes = gtk.SpinButton()
+minutes.set_numeric(True)
+minutes.set_range(0, 59)
+minutes.set_value(40)
+minutes.set_increments(1, 10)
+hbox2.add(minutes)
+hbox2.pack_start(gtk.Label('min'), False)
+
 vbox.add(hbox2)
+
+hbox3 = gtk.HBox()
+hbox3.pack_end(start, False)
+vbox.add(hbox3)
 
 win.add(vbox)
 
